063dbf3ade15f1c70f6c2748ffac0e0bb54f723d
Michael Grenier <michael@grenier.dev>
2022-10-08 13:31:55 -0400

    chore: added AbortSignal to fs interface

--- libpxlr/filesystem/filesystem.ts
+++ libpxlr/filesystem/filesystem.ts
@@ -1,8 +1,8 @@
 export abstract class Filesystem {
-	abstract exists(path: string): Promise<boolean>;
-	abstract list(path: string): AsyncIterableIterator<string>;
-	abstract read(path: string): Promise<ReadableStream>;
-	abstract write(path: string): Promise<WritableStream>;
+	abstract exists(path: string, abortSignal?: AbortSignal): Promise<boolean>;
+	abstract list(path: string, abortSignal?: AbortSignal): AsyncIterableIterator<string>;
+	abstract read(path: string, abortSignal?: AbortSignal): Promise<ReadableStream>;
+	abstract write(path: string, abortSignal?: AbortSignal): Promise<WritableStream>;
 }
 
 export class IOError extends Error {
