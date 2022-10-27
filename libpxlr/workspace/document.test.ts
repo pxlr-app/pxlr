bb040650a4093f5fd7f5edd4cdd17e088719bd99
Michael Grenier <michael@grenier.dev>
2022-10-26 21:34:18 -0400

    refactor: split commit part of document.executeCommand into it's own method

--- libpxlr/workspace/document.test.ts
+++ libpxlr/workspace/document.test.ts
@@ -1,8 +1,10 @@
-import { assertEquals } from "https://deno.land/std@0.158.0/testing/asserts.ts";
+import { assert, assertEquals } from "https://deno.land/std@0.158.0/testing/asserts.ts";
 import { BufferedRepository, Commit, MemoryFilesystem, Reference, Tree } from "../repository/mod.ts";
 import { GroupNode, GroupNodeRegistryEntry, NodeRegistry, NoteNode, NoteNodeRegistryEntry } from "../nodes/mod.ts";
 import { Workspace } from "./workspace.ts";
 import { autoid } from "../autoid.ts";
+import { ZipFilesystem } from "../repository/mod.ts";
+import { DenoFile, Zip } from "../../libzip/mod.ts"
 
 const nodeRegistry = new NodeRegistry();
 nodeRegistry.registerNodeConstructor(NoteNodeRegistryEntry);
@@ -31,21 +33,39 @@ Deno.test("Document", async (t) => {
 		await repository.writeReference(reference);
 		const workspace1 = new Workspace({ repository, nodeRegistry });
 		const document1 = await workspace1.checkoutDocumentAtBranch("main", { shallow: false });
-		await document1.executeCommand(note1.rename("Your note"), "Test <test@test.local>", "Rename My Note");
+		for (let i = 100_000; --i >= 0;) {
+			const note = document1.getNodeById(note1.id)!;
+			document1.executeCommand(note.rename(`My Note ${i}`));
+			if (i % 10_000 === 0) {
+				await document1.commitChanges("Test <test@test.local>", `Rename My Note ${i}`);
+			}
+		}
+		// assertEquals(document1.getNodeById(note1.id)!.name, "My Note 0");
+		// document1.undoCommand();
+		// assertEquals(document1.getNodeById(note1.id)!.name, "My Note 1");
+		// document1.redoCommand();
+		// assertEquals(document1.getNodeById(note1.id)!.name, "My Note 0");
+
+		//await document1.commitChanges("Test <test@test.local>", `Rename My Note`);
+
 		const branch = await workspace1.getBranch("main");
-		const iterHistory = branch.iterHistory();
-		assertEquals((await iterHistory.next()).value.message, "Rename My Note");
-		assertEquals((await iterHistory.next()).value.message, "init");
-		assertEquals((await iterHistory.next()).done, true);
+		for await (const commit of branch.iterHistory()) {
+			console.log(commit.message);
+		}
+		// const iterHistory = branch.iterHistory();
+		// assertEquals((await iterHistory.next()).value.message, "Rename My Note");
+		// assertEquals((await iterHistory.next()).value.message, "init");
+		// assertEquals((await iterHistory.next()).done, true);
 
 		await repository.flushToFilesystem();
 
-		assertEquals(fs.entries.size, 8);
+		// assertEquals(fs.entries.size, 35);
+		// console.log(new TextDecoder().decode(await zip.get("refs/heads/main")));
 
 		const workspace2 = new Workspace({ repository, nodeRegistry });
 		const document2 = await workspace2.checkoutDocumentAtBranch("main", { shallow: false });
 		const note2 = document2.getNodeById(note1.id);
-		assertEquals(note2?.name, "Your note");
+		assertEquals(note2?.name, "My Note 0");
 
 		// await zip.close();
 		// await denoFile.close();
--- libpxlr/workspace/document.ts
+++ libpxlr/workspace/document.ts
@@ -7,12 +7,14 @@ export class Document {
 	#workspace: Workspace;
 	#reference: Reference | undefined;
 	#commit: Commit;
-	#rootNode: Node;
+	#historyRoots: Node[];
+	#historyCursor: number;
 	constructor(workspace: Workspace, reference: Reference | undefined, commit: Commit, rootNode: Node) {
 		this.#workspace = workspace;
 		this.#reference = reference;
 		this.#commit = commit;
-		this.#rootNode = rootNode;
+		this.#historyRoots = [rootNode];
+		this.#historyCursor = 0;
 	}
 
 	get workspace() {
@@ -28,13 +30,21 @@ export class Document {
 	}
 
 	get rootNode() {
-		return this.#rootNode;
+		return this.#historyRoots.at(this.#historyCursor)!;
+	}
+
+	undoCommand() {
+		this.#historyCursor = Math.max(0, this.#historyCursor - 1);
+	}
+
+	redoCommand() {
+		this.#historyCursor = Math.min(this.#historyRoots.length - 1, this.#historyCursor + 1);
 	}
 
 	getNodeByHash(hash: AutoId): Node | undefined {
 		assertAutoId(hash);
-		if (this.#rootNode) {
-			for (const node of this.#rootNode) {
+		if (this.rootNode) {
+			for (const node of this.rootNode) {
 				if (node.hash === hash) {
 					return node;
 				}
@@ -44,8 +54,8 @@ export class Document {
 
 	getNodeById(id: AutoId): Node | undefined {
 		assertAutoId(id);
-		if (this.#rootNode) {
-			for (const node of this.#rootNode) {
+		if (this.rootNode) {
+			for (const node of this.rootNode) {
 				if (node.id === id) {
 					return node;
 				}
@@ -54,37 +64,48 @@ export class Document {
 	}
 
 	getNodeAtNamePath(path: string[]): Node | undefined {
-		if (this.#rootNode && this.#rootNode instanceof GroupNode) {
-			return this.#rootNode.getChildAtNamePath(path);
+		if (this.rootNode && this.rootNode instanceof GroupNode) {
+			return this.rootNode.getChildAtNamePath(path);
 		}
 	}
 
-	async executeCommand(command: Command, author: string, message: string): Promise<Document> {
-		const rootNode = this.#rootNode.executeCommand(command);
-		if (rootNode && rootNode !== this.#rootNode) {
-			let changed = false;
-			const oldNodeSet = new Set();
-			for (const node of this.#rootNode) {
-				oldNodeSet.add(node.hash);
-			}
-			for (const node of rootNode) {
-				if (!oldNodeSet.has(node.hash)) {
-					changed = true;
-					await this.workspace.repository.writeObject(node.toObject());
-				}
-			}
-			if (changed) {
-				const commit = new Commit(autoid(), this.#commit!.hash, rootNode.hash, author, new Date(), message);
-				await this.workspace.repository.writeCommit(commit);
-				if (this.reference) {
-					const reference = new Reference(this.reference.ref, commit.hash);
-					await this.workspace.repository.writeReference(reference);
-				}
-				this.#commit = commit;
-				// TODO onChange callback
-			}
-			this.#rootNode = rootNode;
+	executeCommand(command: Command): Document {
+		const rootNode = this.rootNode.executeCommand(command);
+		if (rootNode !== this.rootNode) {
+			this.#historyRoots.push(rootNode);
+			this.#historyCursor += 1;
+			// TODO onChange callback
 		}
 		return this;
 	}
+
+	async commitChanges(author: string, message: string): Promise<Commit> {
+		const oldRoot = this.#historyRoots.at(0);
+		const newRoot = this.rootNode;
+		if (!oldRoot || oldRoot === newRoot) {
+			throw new NothingToCommitError();
+		}
+
+		const oldNodeSet = new Set();
+		for (const node of oldRoot) {
+			oldNodeSet.add(node.hash);
+		}
+		for (const node of newRoot) {
+			if (!oldNodeSet.has(node.hash)) {
+				await this.workspace.repository.writeObject(node.toObject());
+			}
+		}
+		const commit = new Commit(autoid(), this.#commit!.hash, newRoot.hash, author, new Date(), message);
+		await this.workspace.repository.writeCommit(commit);
+		if (this.reference) {
+			const reference = new Reference(this.reference.ref, commit.hash);
+			await this.workspace.repository.writeReference(reference);
+		}
+		this.#commit = commit;
+		this.#historyRoots.splice(0, this.#historyRoots.length, newRoot);
+		this.#historyCursor = 0;
+		return commit;
+	}
 }
+
+export class NothingToCommitError extends Error {}
