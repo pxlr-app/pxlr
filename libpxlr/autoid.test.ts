2a8c53920be24b07e535c11c0a8485ee016cf675
Michael Grenier <michael@grenier.dev>
2022-10-27 19:17:56 -0400

    fix(pxlr): autoid seed no longer accept number

--- libpxlr/autoid.test.ts
+++ libpxlr/autoid.test.ts
@@ -1,9 +1,9 @@
-import { assertEquals } from "https://deno.land/std@0.158.0/testing/asserts.ts";
+import { assertEquals, assertNotEquals } from "https://deno.land/std@0.158.0/testing/asserts.ts";
 import { autoid } from "./autoid.ts";
 
 Deno.test("AutoID", () => {
-	console.log(autoid());
-	console.log(autoid(0));
-	console.log(autoid(autoid(0)));
-	console.log(autoid(autoid(autoid(0))));
+	assertEquals(autoid().length, 40);
+	assertNotEquals(autoid(), autoid());
+	assertEquals(autoid("iRHeRoI634JxFmKf9EUeFukqcpBAH18TAHTW82NK"), "e0k54WlkOQU6XmhVq36wT91tlGadvtoIkFh8cDAZ");
+	assertEquals(autoid(autoid("iRHeRoI634JxFmKf9EUeFukqcpBAH18TAHTW82NK")), "J0AIOHElG1kbWUCrMVQeX6qc6wMSZo7ec67BmH1f");
 });
--- libpxlr/autoid.ts
+++ libpxlr/autoid.ts
@@ -4,33 +4,20 @@ const AutoIdSize = 40;
 const AutoIdChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 const AutoIdCharsLength = AutoIdChars.length;
 const AutoIdRegExp = new RegExp(`^[${AutoIdChars}]{${AutoIdSize}}$`);
-const textEncoder = new TextEncoder();
 
 /**
  * Generate an AutoId
  * @param seed The seed for the AutoId
  * @returns An AutoId
  */
-export function autoid(): AutoId;
-export function autoid(seed: number): AutoId;
-export function autoid(seed: AutoId): AutoId;
-export function autoid(seed_or_autoid?: number | AutoId): AutoId {
+export function autoid(seed?: AutoId): AutoId {
 	let result = "";
-	if (seed_or_autoid !== undefined) {
-		if (typeof seed_or_autoid === "number") {
-			const rand = sfc32(0x9E3779B9, 0x243F6A88, 0xB7E15162, seed_or_autoid);
-			for (let i = 0; i < AutoIdSize; ++i) {
-				result += AutoIdChars.charAt(rand() * AutoIdCharsLength);
-			}
-		} else if (typeof seed_or_autoid === "string") {
-			assertAutoId(seed_or_autoid);
-			const seed = cyrb128(seed_or_autoid);
-			const rand = sfc32(seed[0], seed[1], seed[2], seed[3]);
-			for (let i = 0; i < AutoIdSize; ++i) {
-				result += AutoIdChars.charAt(rand() * AutoIdCharsLength);
-			}
-		} else {
-			throw new TypeError(`Expected seed parameter to be a number or an AutoId, got ${typeof seed_or_autoid}.`)
+	if (typeof seed === "string") {
+		assertAutoId(seed);
+		const hash = cyrb128(seed);
+		const rand = sfc32(hash[0], hash[1], hash[2], hash[3]);
+		for (let i = 0; i < AutoIdSize; ++i) {
+			result += AutoIdChars.charAt(rand() * AutoIdCharsLength);
 		}
 	} else {
 		const buffer = new Uint8Array(AutoIdSize);
@@ -77,7 +64,7 @@ function sfc32(a: number, b: number, c: number, d: number) {
 		b >>>= 0;
 		c >>>= 0;
 		d >>>= 0;
-		var t = (a + b) | 0;
+		let t = (a + b) | 0;
 		a = b ^ b >>> 9;
 		b = c + (c << 3) | 0;
 		c = c << 21 | c >>> 11;
